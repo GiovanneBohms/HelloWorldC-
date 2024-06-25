4718b87f0258b662df49a2f3b26314c78b72ab01
Giovanne Bohms <giovannebohms@gmail.com>
2024-06-25 14:15:36 -0300

    switch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿string mensagemDeBoasVinda = "boas vindas ao screen sound";
-
+﻿
 void ExibirMensagemDeBoasVindas()
 {
     Console.WriteLine(@"
@@ -23,9 +22,19 @@ void ExibirOpcoesDoMenu()
     Console.Write("\nDigite a sua opção: ");
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-    if (opcaoEscolhidaNumerica == 1)
+
+    switch (opcaoEscolhidaNumerica)
     {
-        Console.WriteLine("Você digitou a opção: " + opcaoEscolhida);
+        case 1: Console.WriteLine("Você escolheu a opção:" + opcaoEscolhidaNumerica);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção:" + opcaoEscolhidaNumerica);
+            break;
+        case 3:Console.WriteLine("Você escolheu a opção:" + opcaoEscolhidaNumerica);
+            break;
+        case 4:Console.WriteLine("Você escolheu a opção:" + opcaoEscolhidaNumerica);
+            break;
+        case -1:Console.WriteLine("Você escolheu a opção:" + opcaoEscolhidaNumerica);
+            break;
     };
 };
 
